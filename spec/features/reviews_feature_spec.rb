263c908ff5dc3b37c4c4c160a42bddee3ef9dd12
phoebe <phoebe.greig@gmail.com>
2016-04-14 17:36:54 +0100

    Users can only leave one review per restaurant

--- spec/features/reviews_feature_spec.rb
+++ spec/features/reviews_feature_spec.rb
@@ -5,6 +5,8 @@ feature 'reviewing' do
 
   scenario 'allows users to leave a review using a form' do
      visit '/restaurants'
+     User.create(email: 'test@example.com', password: 'testtest', password_confirmation: 'testtest')
+     user_sign_in
      click_link 'Review KFC'
      fill_in "Thoughts", with: "so so"
      select '3', from: 'Rating'
@@ -12,6 +14,17 @@ feature 'reviewing' do
 
      expect(current_path).to eq '/restaurants'
      expect(page).to have_content('so so')
+  end
+
+  scenario 'allows users to leave only one review per restaurant' do
+     visit '/restaurants'
+     User.create(email: 'test@example.com', password: 'testtest', password_confirmation: 'testtest')
+     user_sign_in
+     click_link 'Review KFC'
+     fill_in "Thoughts", with: "so so"
+     select '3', from: 'Rating'
+     click_button 'Leave Review'
+     expect(page).not_to have_content('Review KFC')
   end
 
 end
